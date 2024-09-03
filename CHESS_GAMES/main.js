98ed50a4720a536e3814f1183d1fa63e88697b54
Hari Sridhar <hari.srdhr@gmail.com>
2024-09-02 19:37:00 -0700

    Adding directory structure for basic chess game.

---
+++ CHESS_GAMES/main.js
@@ -0,0 +1,12 @@
+/*
+First time coding up a chess javascript game
+Game project decomposition : 
+a. Focus on a single piece  ( a pawn ) and movements.
+Ensure each piece respects boundary conditions.
+b. Extend OOP to other pieces : knights, bishops, rooks, etc., 
+c. set up a game to track score and piece captures
+d. enable ability to restart the game : incorporate a leaderboard???
+*/
+
+
+
